bcb1aff2d80d8f2293cbb2d4298cca76b768dc0f
gabrielaspooky <gabrielanava01@gmail.com>
2024-09-19 22:12:29 +0200

    reinstalled lucide package

--- examen-final-gabriela-nava/components/ui/CardGallery.jsx
+++ examen-final-gabriela-nava/components/ui/CardGallery.jsx
@@ -1,26 +1,26 @@
-"use client";
-import React from "react";
-import { MoveRight } from "lucide-react";
-import Link from "next/link";
-import ProductFetcher from "../api/products/ProductFetcher";
-import Cards from "../../../components/ui/CardProduct";
+// "use client";
+// import React from "react";
+// import { MoveRight } from "lucide-react";
+// import Link from "next/link";
+// import ProductFetcher from "../api/products/ProductFetcher";
+// import Cards from "../../../components/ui/CardProduct";
 
-const ProductCards = () => (
-  <div className="min-h-[603.39px] flex flex-col justify-center items-center gap-10 bg-white">
-    <h2 className="text-2xl font-medium text-[#2A5B45] leading-7 w-[120px] p-10">Novedades</h2>
-    <div className="flex max-w-[1200px] min-h-[391.39px] gap-6 justify-between items-center">
-      <ProductFetcher>
-        {({ products }) => (
+// const ProductCards = () => (
+//   <div className="min-h-[603.39px] flex flex-col justify-center items-center gap-10 bg-white">
+//     <h2 className="text-2xl font-medium text-[#2A5B45] leading-7 w-[120px] p-10">Novedades</h2>
+//     <div className="flex max-w-[1200px] min-h-[391.39px] gap-6 justify-between items-center">
+//       <ProductFetcher>
+//         {({ products }) => (
        
-          <Cards products={Array.isArray(products) ? products.slice(0, 4) : []} />
-        )}
-      </ProductFetcher>
-    </div>
-    <Link className="py-8 flex justify-between items-center gap-4 text-sm font-semibold text-black" href="/shop">
-      Ver todos
-      <MoveRight className="h-6 w-6" />
-    </Link>
-  </div>
-);
+//           <Cards products={Array.isArray(products) ? products.slice(0, 4) : []} />
+//         )}
+//       </ProductFetcher>
+//     </div>
+//     <Link className="py-8 flex justify-between items-center gap-4 text-sm font-semibold text-black" href="/shop">
+//       Ver todos
+//       <MoveRight className="h-6 w-6" />
+//     </Link>
+//   </div>
+// );
 
-export default ProductCards;
+// export default ProductCards;
--- examen-final-gabriela-nava/components/ui/Sidebar.jsx
+++ examen-final-gabriela-nava/components/ui/Sidebar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-// import { Home, Clock, Info, Image } from 'lucide-react';
+import { Home, Clock, Info, Image } from 'lucide-react';
 
 const Sidebar = () => {
 
@@ -11,25 +11,25 @@ const Sidebar = () => {
       <div className="flex flex-col items-start py-4 px-4">
  
         <a href="#card-generator" className="flex items-center mb-4 p-2 w-full text-black hover:bg-green-500">
-          {/* <Home className="mr-2" /> */}
+          <Home className="mr-2" />
           <span>Card Generator</span>
         </a>
 
   
         <a href="#digital-clock" className="flex items-center mb-4 p-2 w-full text-black hover:bg-green-500">
-          {/* <Clock className="mr-2" /> */}
+          <Clock className="mr-2" />
           <span>Digital Clock</span>
         </a>
 
     
         <a href="#drilling-info" className="flex items-center mb-4 p-2 w-full text-black hover:bg-green-500">
-          {/* <Info className="mr-2" /> */}
+          <Info className="mr-2" />
           <span>Drilling Info</span>
         </a>
 
  
         <a href="#card-gallery" className="flex items-center mb-4 p-2 w-full text-black hover:bg-green-500 ">
-          {/* <Image className="mr-2" /> */}
+          <Image className="mr-2" />
           <span>Galería de Cartas</span>
         </a>
       </div>
